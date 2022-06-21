fc44ca5a480032e92069abc447eac582dd3b1671
Mohit Dodhia <mhtdodhia@gmail.com>
2022-06-21 22:28:36 +0530

    Solved problem 2235

---
+++ src/leetcode/easy/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+package leetcode.easy;
+
+// PROBLEM NO. 2235
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(new AddTwoIntegers().sum(-10, 4));
+    }
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
